f516ede8ec6109ceae3636a7a1ea0a672e7f3346
mujeeb <muzeeb100@yahoo.co.in>
2022-03-30 15:54:01 +0530

    initailize repository

--- starter/script.js
+++ starter/script.js
@@ -1,30 +1,23 @@
-'use strict';
+"use strict";
 // Modal applicatoin in javaScript
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
 
 const openModal = function () {
-  console.log('Button clicked');
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener("click", openModal);
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-//click on button to open the model
-for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener('click', openModal);
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-// //side button click
-// overlay.addEventListener('click', function(){
-//     modal.classList.add('hidden');
-//     overlay.classList.add('hidden');
-// })
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
